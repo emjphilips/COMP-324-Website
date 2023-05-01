fa1c984b10320627a5d52bb877503fe992496690
emjphilips <94873763+emjphilips@users.noreply.github.com>
2023-05-01 15:44:14 -0500

    Add files via upload

---
+++ SearchFunctionality.js
@@ -0,0 +1,23 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3175a60233msh10bd93a81a5f8b0p171adfjsnd5e2df15191d',
+		'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+	}
+};
+
+async function searchPlace(){
+	const searchText = document.getElementById("place").value;
+	localStorage.setItem('placeName',searchText);
+	const res = await fetch(`https://travel-advisor.p.rapidapi.com/locations/search?query=${searchText}&limit=4`, options)
+	.then(response => response.json());
+	const firstLocationId = res.data[0].result_object.location_id;
+	const places = await fetch(`https://travel-advisor.p.rapidapi.com/attractions/list?location_id=${firstLocationId}&limit=6`, options)
+	.then(response => response.json());
+	console.log(places);
+	
+	localStorage.setItem('apiData',JSON.stringify(places.data));
+	window.location.href= 'places.html';
+	//showData();
+	
+}
